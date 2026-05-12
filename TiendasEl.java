4163fa195b8657b6503d0ebc44c942c891106ee4
Dennis Ronaldo <dennisccamasaico@gmail.com>
2026-05-12 18:04:51 -0500

    Update TiendasEl.java
    primer avance

---
+++ TiendasEl.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tiendas_el {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido a TIENDAS EL");
+        System.out.println("COMPRA HASTA 3 CUOTAS SIN INTERESES CON BBVA Y DINERS* exclusivo online");
+        System.out.println("DELIVERY GRATIS A LIMA METROPOLITANA POR COMPRAS SUPERIORES A S/299.9");
+        System.out.println("Compras por Whatsapp +51 989 174 974");
+    }
+}
